d2b16c74fbe68e8d3a3cb78e2e3566f83ef2bb60
Carlos Motiño <carlos.motino911@gmail.com>
2021-04-28 21:26:15 -0600

    First commit

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+import {AddCategory} from './components/AddCategory';
+import { GiftGrid } from './components/GiftGrid';
+export const GiftExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['Dragon Ball']);
+    /*const handleAdd = () =>{
+        const newCategorie = 'One Piece';
+        //setCategories([...categories,newCategorie]);
+        setCategories(cats => [...cats,newCategorie]);
+    }*/
+
+
+
+    return (
+        <>
+          <h2>GifExpertApp</h2>  
+          <hr/>
+          <AddCategory setCategories={setCategories} />
+          <ol>
+              { categories.map( category =>(
+                  <GiftGrid
+                    key={category}
+                    category={category}
+                  />
+              )) 
+              }
+          </ol>
+        </>
+    )
+}
---
+++ src/components/GiftGrid.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GrifGridItem } from './GrifGridItem';
+
+export const GiftGrid = ({category}) => {
+    const {data:images,loading} = useFetchGifs(category);
+    
+
+    /*const [images, setImages] = useState([])
+    useEffect(() =>{
+        getGifs(category)
+        .then(setImages);
+    },[category]) */
+
+    return (
+        <>
+            <h3 className="animate__animated animate__fadeIn">{category}</h3>
+            {loading && <p className="animate__animated animate__flash">Loading...</p>}
+            <div className="card-grid">            
+                {
+                    images.map(img =>(
+                        <GrifGridItem 
+                            key={img.id}
+                            {...img} />
+                    ))
+                }            
+            </div>
+        </>
+        
+    )
+}
